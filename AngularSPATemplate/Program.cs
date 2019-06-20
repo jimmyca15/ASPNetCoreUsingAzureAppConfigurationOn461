32a5444a9dbb7f7562f0be28ec7ec1183ed117aa
Jimmy Campbell <jimmyca@microsoft.com>
2019-06-20 16:34:33 -0400

    Added angular template project.

---
+++ AngularSPATemplate/Program.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using Microsoft.Extensions.Logging;
+
+namespace AngularSPATemplate
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(builder => {
+
+                    IConfiguration intermediateConfig = builder.Build();
+
+                    string appConfigUri = intermediateConfig["AzureAppConfigurationUri"];
+
+                    if (!string.IsNullOrEmpty(appConfigUri))
+                    {
+                        builder.AddAzureAppConfiguration(o => o.ConnectWithManagedIdentity(appConfigUri));
+                    }
+
+                })
+                .UseStartup<Startup>();
+    }
+}
