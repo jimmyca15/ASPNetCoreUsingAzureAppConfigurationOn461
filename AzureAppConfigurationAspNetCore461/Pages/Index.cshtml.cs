a156af0964b2c34a17cc778070f0e850dacb5c45
Jimmy Campbell <jimmyca@microsoft.com>
2019-06-20 13:25:46 -0400

    ASP.NET Core 2.2 app targeting NetFX 4.6.1. Uses Azure App Configuration as an IConfiguration provider.

---
+++ AzureAppConfigurationAspNetCore461/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace AzureAppConfigurationAspNetCore461.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public const string MessageKey = "Message";
+
+        private IConfiguration _configuration;
+
+        public IndexModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        public void OnGet()
+        {
+            ViewData[MessageKey] = _configuration[MessageKey];
+        }
+    }
+}
